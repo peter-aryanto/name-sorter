e5e173be6cdbcf624f179faf812183fc94281702
Peter Aryanto <peter.aryanto@gmail.com>
2017-10-29 22:12:36 +1100

    Adding Rules and Methods in PersonName Class

--- Person.Name/PersonName.cs
+++ Person.Name/PersonName.cs
@@ -9,6 +9,39 @@ namespace Person.Name
 
     public PersonName(string fullName)
     {
+      FullNameCannotBeEmpty(fullName);
+
+      string[] nameParts = GetAllNameParts(fullName);
+
+      FullNameMustHaveAtLeastOneGivenNameAndOneLastName(nameParts);
+      FullNameMayHaveUpToThreeGivenNamesAndOneLastName(nameParts);
+
+      GivenNames = GetGivenNamesFromFullName(nameParts);
+      LastName = nameParts[nameParts.Length - 1];
+    }
+
+    private void FullNameCannotBeEmpty(string fullName)
+    {
+      throw new NotImplementedException("There has been no implementation yet.");
+    }
+
+    private string[] GetAllNameParts(string fullName)
+    {
+      throw new NotImplementedException("There has been no implementation yet.");
+    }
+
+    private void FullNameMustHaveAtLeastOneGivenNameAndOneLastName(string[] nameParts)
+    {
+      throw new NotImplementedException("There has been no implementation yet.");
+    }
+
+    private void FullNameMayHaveUpToThreeGivenNamesAndOneLastName(string[] nameParts)
+    {
+      throw new NotImplementedException("There has been no implementation yet.");
+    }
+
+    private string GetGivenNamesFromFullName(string[] nameParts)
+    {
       throw new NotImplementedException("There has been no implementation yet.");
     }
   }
