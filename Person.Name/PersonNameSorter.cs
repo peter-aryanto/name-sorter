c2c4768bdf2ec7a9f9d387e37e10b082648d69b9
Peter Aryanto <peter.aryanto@gmail.com>
2017-10-29 23:33:59 +1100

    Adding PersonNameSorter Class

---
+++ Person.Name/PersonNameSorter.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Person.Name
+{
+  public class PersonNameSorter
+  {
+    public void CaseInsensitiveSort(List<PersonName> personNameListToSort)
+    {
+      throw new NotImplementedException("There has been no implementation yet.");
+    }
+  }
+}
