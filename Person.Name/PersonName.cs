93777e635b207fbfaf17d4200885df21b1170477
Peter Aryanto <peter.aryanto@gmail.com>
2017-10-29 21:54:28 +1100

    Starting Implementation of PersonName Class

---
+++ Person.Name/PersonName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Person.Name
+{
+  public class PersonName
+  {
+    public string GivenNames { get; }
+    public string LastName { get; }
+
+    public PersonName(string fullName)
+    {
+      throw new NotImplementedException("There has been no implementation yet.");
+    }
+  }
+}
